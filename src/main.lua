9897bab5d419423af8114de454fd5d5df6afaf8b
Arkadiusz Szolc <Arkadiusz@Szo.lc>
2025-03-05 02:29:14 +0100

    test

--- src/main.lua
+++ src/main.lua
@@ -34,39 +34,44 @@ local TrackLines = Widget:extend{
     settings = nil,
     ALMOST_CENTER_OF_THE_SCREEN = 0.37,
     last_screen_mode = nil,
-    increment = 20  -- Changed from 40 to make movement more precise
+    increment = 40
 }
 
 function TrackLines:onDispatcherRegisterActions()
-    -- Renamed actions to more accurately describe horizontal line movement
-    Dispatcher:registerAction("tracklines_move_up", { category="none", event="TrackLinesMoveUp", title=_("Move line up"), general=true,})
-    Dispatcher:registerAction("tracklines_move_down", { category="none", event="TrackLinesMoveDown", title=_("Move line down"), general=true,})
+    Dispatcher:registerAction("tracklines_move_up", { category="none", event="TrackLinesMoveUp", title=_("Move up"), general=true,})
+    Dispatcher:registerAction("tracklines_move_down", { category="none", event="TrackLinesMoveDown", title=_("Move down"), general=true,})
 end
 
 function TrackLines:onTrackLinesMoveUp()
-    -- Move the line up (decrease y position)
-    self.top_line.dimen.y = self.top_line.dimen.y - self.increment
-    logger.warn("TrackLines moving line up, now render...")
+    self.left_line.dimen.y = self.left_line.dimen.y - self.increment
+    logger.warn("TrackLines decrement y, now render...")
     
+    -- UIManager:setDirty (widget, refreshtype, refreshregion, refreshdither)
+    -- UIManager:forceRePaint()
+    -- UIManager:widgetRepaint (self.left_line, self.left_line.dimen.x, self.left_line.dimen.y)
+    --UIManager:setDirty(self.left_line.dimen, "ui")
     UIManager:setDirty(self.view.dialog, "partial")
 
-    logger.warn("TrackLines move line up, done rendering...")
+    --UIManager:forceRePaint()
+
+    logger.warn("TrackLines decrement y, done rendering...")
     return true
 end
 
 function TrackLines:onTrackLinesMoveDown()
-    -- Move the line down (increase y position)
-    self.top_line.dimen.y = self.top_line.dimen.y + self.increment
-    logger.warn("TrackLines moving line down, now render...")
+    self.left_line.dimen.y = self.left_line.dimen.y + self.increment
+    logger.warn("TrackLines increment y, now render...")
 
+    --UIManager:setDirty(self.left_line.dimen, "ui")
     UIManager:setDirty(self.view.dialog, "partial")
+    --UIManager:forceRePaint()
 
-    logger.warn("TrackLines move line down, done rendering...")
+    logger.warn("TrackLines increment y, done rendering...")
     return true
 end
 
 function TrackLines:init()
-    logger.warn("TrackLines initializing")
+    logger.warn("TrackLines HELLO")
     if not self.settings then self:readSettingsFile() end
     self.is_enabled = self.settings:isTrue("is_enabled")
     if not self.is_enabled then
@@ -79,6 +84,76 @@ function TrackLines:readSettingsFile()
     self.settings = LuaSettings:open(DataStorage:getSettingsDir() .. "/track_lines.lua")
 end
 
+function TrackLines:createUI(readSettings)
+    logger.warn("TrackLines about to register and create UI")
+    self:onDispatcherRegisterActions()
+    if readSettings then
+        self.line_thickness = tonumber(self.settings:readSetting("line_thick"))
+        self.margin = tonumber(self.settings:readSetting("margin"))
+        self.line_color_intensity = tonumber(self.settings:readSetting("line_color_intensity"))
+        self.shift_each_pages = tonumber(self.settings:readSetting("shift_each_pages"))
+        self.page_counter = tonumber(self.settings:readSetting("page_counter"))
+    end
+
+    self.screen_width = Screen:getWidth()
+
+    -- TODO: this is setting line height - we want width
+    local screen_height = Screen:getHeight()
+    local line_height = math.floor(screen_height * 0.9)
+    local line_top_position = math.floor(screen_height * 0.05)
+
+    self.last_screen_mode = Screen:getScreenMode()
+    if self.last_screen_mode == "landscape" then
+        self.margin = (self.margin - self.margin_shift)
+    end
+
+    local line_widget = LineWidget:new{
+       background = Blitbuffer.gray(self.line_color_intensity),
+
+       -- TODO: is this where position is set? can we make this horizontal??
+        dimen = Geom:new{
+            w = line_height,
+            h = self.line_thickness,
+        },
+    }
+
+    self.left_line = WidgetContainer:new{
+       -- TODO: is this where position is set? can we make this horizontal??
+        dimen = Geom:new{
+            x = 0, --self.screen_width * self.margin,
+            y = line_top_position,
+            w = self.line_thickness,
+            h = line_height,
+        },
+        line_widget
+    }
+
+    -- self.right_line = WidgetContainer:new{
+    --    -- TODO: is this where position is set? can we make this horizontal??
+    --     dimen = Geom:new{
+    --         x = self.screen_width - (self.screen_width * self.margin),
+    --         y = line_top_position,
+    --         w = self.line_thickness,
+    --         h = line_height,
+    --     },
+    --     line_widget
+    -- }
+
+    self[1] = HorizontalGroup:new{
+        self.left_line,
+        -- self.right_line,
+    }
+end
+
+function TrackLines:onReaderReady()
+    self.ui.menu:registerToMainMenu(self)
+    self.view:registerViewModule("track_lines", self)
+end
+
+function TrackLines:resetLayout()
+    self:createUI()
+end
+
 function TrackLines:showSettingsDialog()
     self.settings_dialog = MultiInputDialog:new{
         title = _("Track lines settings"),
@@ -86,7 +161,7 @@ function TrackLines:showSettingsDialog()
             {
                 text = "",
                 input_type = "number",
-                hint = T(_("Line thickness. Current value: %1\nChanging requires restart."),
+                hint = T(_("Line thickness. Current value: %1"),
                     self.line_thickness),
             },
             {
@@ -121,38 +196,10 @@ function TrackLines:showSettingsDialog()
                 {
                     text = _("Apply"),
                     callback = function()
-                        -- Get the input fields
-                        local fields = MultiInputDialog:getFields()
-                        
-                        -- Save current line thickness before changing
-                        local old_thickness = self.line_thickness
-                        
-                        -- Save all settings
-                        self:saveSettings(fields)
-                        
-                        -- Close dialog first
+                        self:saveSettings(MultiInputDialog:getFields())
                         self.settings_dialog:onClose()
                         UIManager:close(self.settings_dialog)
-                        
-                        -- Check if thickness changed
-                        if old_thickness ~= self.line_thickness then
-                            -- Thickness changed - show restart required message
-                            UIManager:show(InfoMessage:new{
-                                text = _("Line thickness has been changed. This change will take effect after restarting KOReader."),
-                                timeout = 3,
-                            })
-                            
-                            -- Reset to old thickness for current session
-                            self.line_thickness = old_thickness
-                            
-                            -- Only update other settings
-                            self:resetWidget()
-                            UIManager:setDirty(nil, "ui")
-                        else
-                            -- If only other settings changed, just recreate UI
-                            self:resetWidget()
-                            UIManager:setDirty(nil, "ui")
-                        end
+                        self:createUI()
                     end
                 },
             },
@@ -162,105 +209,9 @@ function TrackLines:showSettingsDialog()
     self.settings_dialog:onShowKeyboard()
 end
 
--- New method to completely reset the widget
-function TrackLines:resetWidget()
-    logger.warn("TrackLines completely resetting widget")
-    
-    -- Remove old widget
-    self[1] = nil
-    
-    -- Create new widget from scratch
-    self:createUI(false)
-    
-    logger.warn("TrackLines widget reset complete")
-end
-
-function TrackLines:createUI(readSettings)
-    logger.warn("TrackLines creating UI")
-    self:onDispatcherRegisterActions()
-    if readSettings then
-        self.line_thickness = tonumber(self.settings:readSetting("line_thick")) or self.line_thickness
-        self.margin = tonumber(self.settings:readSetting("margin")) or self.margin
-        self.line_color_intensity = tonumber(self.settings:readSetting("line_color_intensity")) or self.line_color_intensity
-        self.shift_each_pages = tonumber(self.settings:readSetting("shift_each_pages")) or self.shift_each_pages
-        self.page_counter = tonumber(self.settings:readSetting("page_counter")) or 0
-    end
-
-    -- Ensure line thickness is valid (at least 1 pixel)
-    if not self.line_thickness or self.line_thickness < 1 then
-        self.line_thickness = 1
-        logger.warn("TrackLines invalid line thickness, setting to 1")
-    elseif self.line_thickness > 20 then -- Set a reasonable maximum
-        self.line_thickness = 20
-        logger.warn("TrackLines line thickness too large, setting to 20")
-    end
-
-    -- Log current line thickness for debugging
-    logger.warn("TrackLines using line thickness: " .. self.line_thickness)
-
-    self.screen_width = Screen:getWidth()
-    local screen_height = Screen:getHeight()
-    
-    -- Create horizontal line that spans the screen width
-    local line_width = self.screen_width
-    local line_position_y = math.floor(screen_height * 0.3) -- Position at 30% down the screen
-
-    self.last_screen_mode = Screen:getScreenMode()
-    if self.last_screen_mode == "landscape" then
-        self.margin = (self.margin - self.margin_shift)
-    end
-
-    -- Create a new clean line widget
-    local line_thickness = math.floor(self.line_thickness) -- Ensure integer value
-    
-    -- KOReader approach: create a clean BB first
-    local line_bb = Blitbuffer.new(line_width, line_thickness, Blitbuffer.TYPE_BB8)
-    -- Fill it with the appropriate color
-    line_bb:fill(Blitbuffer.gray(self.line_color_intensity))
-    
-    -- Create a horizontal line widget with the prepared BB
-    local line_widget = Widget:new{
-        dimen = Geom:new{
-            w = line_width,
-            h = line_thickness,
-        },
-        -- Override the paintTo method to use our prepared BB
-        paintTo = function(self, bb, x, y)
-            bb:blitFrom(line_bb, x, y)
-        end,
-    }
-
-    -- Create a container for the horizontal line
-    self.top_line = WidgetContainer:new{
-        dimen = Geom:new{
-            x = 0,
-            y = line_position_y,
-            w = line_width,
-            h = line_thickness,
-        },
-        line_widget
-    }
-
-    -- Create and assign the container
-    self[1] = HorizontalGroup:new{
-        self.top_line,
-    }
-    
-    logger.warn("TrackLines UI creation completed with thickness: " .. line_thickness)
-end
-
-function TrackLines:onReaderReady()
-    self.ui.menu:registerToMainMenu(self)
-    self.view:registerViewModule("track_lines", self)
-end
-
-function TrackLines:resetLayout()
-    self:resetWidget()
-end
-
 function TrackLines:addToMainMenu(menu_items)
     menu_items.speed_reading_module_track_lines = {
-        text = _("Reading Guide Lines"),  -- Improved menu text
+        text = _("horizontal lines"),
         sub_item_table ={
             {
                 text = _("Enable"),
@@ -280,29 +231,12 @@ function TrackLines:addToMainMenu(menu_items)
                 end,
             },
             {
-                text = _("Move Line Up"),  -- Added menu option to move line up
-                keep_menu_open = true,
-                callback = function()
-                    if self.is_enabled and self.top_line then
-                        self:onTrackLinesMoveUp()
-                    end
-                end,
-            },
-            {
-                text = _("Move Line Down"),  -- Added menu option to move line down
-                keep_menu_open = true,
-                callback = function()
-                    if self.is_enabled and self.top_line then
-                        self:onTrackLinesMoveDown()
-                    end
-                end,
-            },
-            {
                 text = _("About"),
                 keep_menu_open = true,
                 callback = function()
+                   -- TODO: MAYBE COPY THIS AND MAKE IT INCREMENT OR DECREMENT LINE WIDTH
                     UIManager:show(InfoMessage:new{
-                        text = _("Reading Guide Lines helps track your position while reading by displaying horizontal guide lines."),
+                        text = _("For more information....too bad."),
                     })
                 end,
             },
@@ -311,43 +245,37 @@ function TrackLines:addToMainMenu(menu_items)
 end
 
 function TrackLines:onPageUpdate(pageno)
+
     logger.warn("TrackLines on page update")
 
-    if not self.is_enabled then
-        return
-    end
+   --if not self.is_enabled then
+   --     return
+   -- end
 
-    -- Reset line position to default when changing pages
-    self.top_line.dimen.y = math.floor(Screen:getHeight() * 0.3)
+    self.left_line.dimen.y = math.floor(Screen:getHeight() * 0.05)
     logger.warn("TrackLines reset line position")
     UIManager:setDirty(self.view.dialog, "partial")
+
     
     -- If this plugin did not apply screen orientation change, redraw plugin UI
     if Screen:getScreenMode() ~= self.last_screen_mode then
         self:createUI()
     end
 
-    -- Update page counter logic
     if self.shift_each_pages ~= 0 and self.page_counter >= self.shift_each_pages and self.margin < self.ALMOST_CENTER_OF_THE_SCREEN then
         self.page_counter = 0
         self.margin = self.margin + self.margin_shift
+        -- self.left_line.dimen.x = self.screen_width * self.margin
+        -- self.right_line.dimen.x = self.screen_width - (self.screen_width * self.margin)
     else
         self.page_counter = self.page_counter + 1;
     end
 end
 
+
 function TrackLines:saveSettings(fields)
-    -- If fields are provided, use them to update values
     if fields then
-        -- Validate line thickness before saving
-        local new_thickness = fields[1] ~= "" and tonumber(fields[1]) or self.line_thickness
-        if new_thickness and new_thickness >= 1 and new_thickness <= 20 then
-            self.line_thickness = new_thickness
-        else
-            logger.warn("TrackLines invalid line thickness input:", new_thickness)
-            -- Keep current value if invalid
-        end
-        
+        self.line_thickness = fields[1] ~= "" and tonumber(fields[1]) or self.line_thickness
         self.margin = fields[2] ~= "" and tonumber(fields[2]) or self.margin
 
         local line_intensity = fields[3] ~= "" and tonumber(fields[3]) or self.line_color_intensity * 10
@@ -357,21 +285,18 @@ function TrackLines:saveSettings(fields)
         self.shift_each_pages = fields[4] ~= "" and tonumber(fields[4]) or self.shift_each_pages
     end
 
-    -- Always save all current settings
     self.settings:saveSetting("line_thick", self.line_thickness)
     self.settings:saveSetting("margin", self.margin)
     self.settings:saveSetting("line_color_intensity", self.line_color_intensity)
     self.settings:saveSetting("shift_each_pages", self.shift_each_pages)
-    self.settings:saveSetting("page_counter", self.page_counter)
     self.settings:saveSetting("is_enabled", self.is_enabled)
     self.settings:flush()
-    
-    logger.warn("TrackLines settings saved, line thickness: " .. self.line_thickness)
+
+    self:createUI()
 end
 
 function TrackLines:paintTo(bb, x, y)
     if self.is_enabled and self[1] then
-        logger.warn("TrackLines painting to screen with thickness: " .. self.line_thickness)
         self[1]:paintTo(bb, x, y)
     end
 end
